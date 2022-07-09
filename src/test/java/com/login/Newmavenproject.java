172dae424d82c48bf75dffe8a96880fad371b637
ashokvijay <ashokvijay206@gmail.com>
2022-07-09 11:22:49 +0530

    message

---
+++ src/test/java/com/login/Newmavenproject.java
@@ -0,0 +1,23 @@
+package com.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Newmavenproject {
+
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+
+}
